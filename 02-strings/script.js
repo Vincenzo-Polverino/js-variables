0ddebfe8a5d0a20e03ef00b71170d5bf5689609c
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-09-27 15:33:31 +0200

    string 3 complete  and fix string 2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -12,7 +12,7 @@ console.log(my_name);
 
 let number= 1;
 let text= 'testo'
-let bool= true | false;
+let bool= true;
 
 console.log(number, text, bool);
 
@@ -24,7 +24,7 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = `${firstName} ${lastName}`;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
